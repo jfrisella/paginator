8382ad5304ca6fc76955e689a94b0907ba15ca79
James Frisella <jafrisella@gmail.com>
2017-05-02 11:11:52 -0400

    changes

--- jquery.paginator.js
+++ jquery.paginator.js
@@ -20,19 +20,27 @@
             *   allows client to do pagination or server
             *   client and server have different data layouts
             */
-            control: "client",
+            pagination: "client",
 
             /**
             *   allows override of return data
             */
             parse: function(data){
-                return (!data || !data.resultSet || !(data.resultSet instanceof Array))
-                    ? [] : data.resultSet;
+                return (!data || !data.resultSet) ? [] : data.resultSet;
             },
 
             render: function(item){
                 console.log("You must override the 'render' function");
                 return "<div class=''>" + JSON.stringify(item) + "</div>";
+            },
+
+            renderNav: function(currentPage, lastPage){
+                var html = "";
+                html += "<div class='row'><div class='col-xs-12'>";
+                html += "<div class='btn-group'>";
+
+                html+= "</div>";
+                html += "</div></div>";
             }
         };
 
@@ -55,16 +63,62 @@
             _private.lastPage = null;
         }
 
+
+        /**
+        *   Page rendering functions
+        */
         _private.buildPage = function(){
             console.log("buildPage");
-            console.log(_private.offset);
+
+            //All data is good, set the last page
+            _private.setLastPage();
+
+            var html = "";
+            _private.buildNavigation();
+            html += _private.buildBody();
+            _private.$el.html(html);
+        }
+        _private.buildBody = function(){
             var html = "";
             for(var i = 0; i < _private.currentData.rows.length; i+=1){
                 html += _options.render(_private.currentData.rows[i]);
             }
-            _private.$el.html(html);
+            return html;
+        }
+        _private.buildNavigation = function(){
+            return _options.renderNav(_private.page, _private.lastPage);
         }
 
+
+        _private.run = function(){
+            _private.fetch(function(){
+                _private.buildPage();
+            });
+        }
+
+        _private.fetch = function(callBack){
+            _private.setOffset();
+
+            if(_options.data){
+                _private.parseClient();
+                return callBack();
+            }
+            $.get(_options.url, _private.parseFetchParams())
+              .done(function(results){
+                  var d = _options.parse(results);
+                  if(_options.pagination === "client"){
+                      _options.data = d;
+                      _private.parseClient();
+                  }else{
+                      _private.currentData = d;
+                  }
+                  return callBack();
+              })
+              .fail(function(x, h, r){
+                  console.log(r);
+                  $.error("fetch : failed to get data from " + _options.url);
+              });
+        }
         _private.parseClient = function(){
             var end = _private.offset + _options.pageSize,
                 end = (_options.data.length < end) ? _options.data.length : end;
@@ -73,44 +127,19 @@
                 rows: _options.data.slice(_private.offset - 1, end - 1)
             }
         }
-
-        _private.setLastPage = function(){
-            _private.lastPage = Math.ceil(_private.currentData.total / _options.pageSize);
-        }
-        _private.setOffset = function(){
-            _private.offset = ((_private.page-1) * _options.pageSize) + 1;
-        }
-
         _private.parseFetchParams = function(){
-            if(_options.control === "client") return _options.params;
+            if(_options.pagination === "client") return _options.params;
             return $.extend({}, _options.params, {
                 limit: _options.pageSize,
                 offset: _private.offset
             });
         }
 
-        _private.fetch = function(){
-            if(_options.data){
-              _private.parseClient();
-              _private.setLastPage();
-              _private.buildPage();
-              return;
-            }
-            $.get(_options.url, _private.parseFetchParams())
-              .done(function(results){
-                  var d = _options.parse(results);
-                  if(_options.control === "client"){
-                      _options.data = d;
-                      _private.parseClient();
-                  }else{
-                      _private.currentData = d;
-                  }
-                  _private.setLastPage();
-                  _private.buildPage();
-              })
-              .fail(function(x, h, r){
-                  $.error("fetch : failed to get data from " + _options.url);
-              });
+        _private.setLastPage = function(){
+            _private.lastPage = Math.ceil(_private.currentData.total / _options.pageSize);
+        }
+        _private.setOffset = function(){
+            _private.offset = ((_private.page-1) * _options.pageSize) + 1;
         }
 
 
@@ -132,42 +161,35 @@
                 $.extend(_options, _defaults, options);
 
                 //Validation checking
-                if(false){
-                //Do some data checks
-                  $.error("init : data : caption data is required");
-                  return false;
+                if(!_options.data && !_options.url){
+                    return $.error("init : data/url : [data] or [url] is required");
                 }
 
                 //preping variables
-                _options.control = (_options.control !== "server") ? "client" : _options.control;
-                _private.setOffset();
+                _options.pagination = (_options.pagination !== "server") ? "client" : _options.pagination;
 
-                _private.fetch();
-
+                _private.run();
             },
 
             setPage: function(page){
                 console.log("setPage");
                 if(page < 1 || page > _private.lastPage) return $.error("setPage : invalid page number");
                 _private.page = page;
-                _private.setOffset();
-                _private.fetch();
+                _private.run();
             },
 
             nextPage: function(){
                 console.log("nextPage");
                 if(_private.page >= _private.lastPage) return $.error("nextPage : there is no next page");
                 _private.page += 1
-                _private.setOffset();
-                _private.fetch();
+                _private.run();
             },
 
             prevPage: function(){
                 console.log("prevPage");
                 if(_private.page <= 1) return $.error("prevPage : there is no previous page");
                 _private.page -= 1;
-                _private.setOffset();
-                _private.fetch();
+                _private.run();
             }
         };
 
