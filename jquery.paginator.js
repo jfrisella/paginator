60e76ba68caa228c1b6f7b39a54e58483d2afe35
Grayson Hicks <graysonhicks@gmail.com>
2017-06-27 09:28:43 -0400

    added on page change method for custom behavior on page change

--- jquery.paginator.js
+++ jquery.paginator.js
@@ -1,293 +1,289 @@
 /**
 *   Paginator
 */
-(function($){
+(function($) {
+	//Nest inside function
+	var _public = (function() {
+		var _options = {},
+			_private = {};
+		var _defaults = {
+			url: null,
+			data: null,
+			params: null,
+			pageSize: 20,
 
-    //Nest inside function
-    var _public = (function(){
+			btnDefault: "btn-default",
+			btnActive: "btn-primary",
 
-        var _options = {},
-            _private = {};
-        var _defaults = {
-
-            url: null,
-            data: null,
-            params: null,
-            pageSize: 20,
-
-            btnDefault: "btn-default",
-            btnActive: "btn-primary",
-
-            /**
+			/**
             *   client, server
             *   allows client to do pagination or server
             *   client and server have different data layouts
             */
-            pagination: "client",
+			pagination: "client",
 
-            /**
+			/**
             *   allows override of return data
             */
-            parse: function(data){
-                return (!data || !data.resultSet) ? [] : data.resultSet;
-            },
+			parse: function(data) {
+				return !data || !data.resultSet ? [] : data.resultSet;
+			},
 
-            render: function(item){
-                console.log("You must override the 'render' function");
-                return "<div class=''>" + JSON.stringify(item) + "</div>";
-            },
+			render: function(item) {
+				console.log("You must override the 'render' function");
+				return "<div class=''>" + JSON.stringify(item) + "</div>";
+			},
 
-            renderEmptyBody: function(){
-                return "<div class='well'>Sorry! There were no results.</div>";
-            },
+			renderEmptyBody: function() {
+				return "<div class='well'>Sorry! There were no results.</div>";
+			},
 
-            renderEmptyPagination: function(){
-                return "<div></div>";
-            },
+			renderEmptyPagination: function() {
+				return "<div></div>";
+			},
 
-            renderLoading: function(){
-                return "<div class='well'>Loading . . .</div>";
-            }
-        };
+			renderLoading: function() {
+				return "<div class='well'>Loading . . .</div>";
+			},
 
+			onPageChange: function(e, page) {
+				// option for behavior on page change
+			}
+		};
 
-        //current page
-        _private.$el;
-        _private.page;
-        _private.offset;
+		//current page
+		_private.$el;
+		_private.page;
+		_private.offset;
 
-        //holds current data for page
-        _private.currentData;
+		//holds current data for page
+		_private.currentData;
 
-        //holds number of last page
-        _private.lastPage;
+		//holds number of last page
+		_private.lastPage;
 
+		_private.reset = function() {
+			_private.page = 1;
+			_private.currentData = null;
+			_private.lastPage = null;
+		};
 
-        _private.reset = function(){
-            _private.page = 1;
-            _private.currentData = null;
-            _private.lastPage = null;
-        }
-
-
-        /**
+		/**
         *   Page rendering functions
         */
-        _private.buildPage = function(){
-            console.log("buildPage");
+		_private.buildPage = function() {
+			console.log("buildPage");
 
-            //All data is good, set the last page
-            _private.setLastPage();
+			//All data is good, set the last page
+			_private.setLastPage();
 
-            var html = "<div id='paginator-main-container'>";
-            html += _private.buildNavigation();
-            html += _private.buildBody();
-            html += _private.buildNavigation();
-            html += "</div>";
-            _private.$el.html(html);
+			var html = "<div id='paginator-main-container'>";
+			html += _private.buildNavigation();
+			html += _private.buildBody();
+			html += _private.buildNavigation();
+			html += "</div>";
+			_private.$el.html(html);
 
-            _private.changeActive();
-        }
-        _private.buildBody = function(){
-            var html = "<div class='row paginator-body-container'><div class='col-xs-12'>";
-            if(!_private.currentData.rows || _private.currentData.rows.length === 0){
-                html += _options.renderEmptyBody();
-            }else{
-                for(var i = 0; i < _private.currentData.rows.length; i+=1){
-                    html += _options.render(_private.currentData.rows[i]);
-                }
-            }
-            html += "</div></div>";
-            return html;
-        }
-        _private.buildNavigation = function(){
-              var html = "",
-                  currentPage = _private.page,
-                  lastPage = _private.lastPage;
+			_private.changeActive();
+		};
+		_private.buildBody = function() {
+			var html = "<div class='row paginator-body-container'><div class='col-xs-12'>";
+			if (!_private.currentData.rows || _private.currentData.rows.length === 0) {
+				html += _options.renderEmptyBody();
+			} else {
+				for (var i = 0; i < _private.currentData.rows.length; i += 1) {
+					html += _options.render(_private.currentData.rows[i]);
+				}
+			}
+			html += "</div></div>";
+			return html;
+		};
+		_private.buildNavigation = function() {
+			var html = "",
+				currentPage = _private.page,
+				lastPage = _private.lastPage;
 
-              html += "<div class='row paginator-pagination-container'><div class='col-xs-12'>";
-              html += "<div class='btn-group pull-right'>";
-              html += "<a href='' class='btn " + _options.btnDefault + "' id='paginator-prev-page'>&#60;</a>";
+			html += "<div class='row paginator-pagination-container'><div class='col-xs-12'>";
+			html += "<div class='btn-group pull-right'>";
+			html += "<a href='' class='btn " + _options.btnDefault + "' id='paginator-prev-page'>&#60;</a>";
 
-              if(lastPage >= 1){
+			if (lastPage >= 1) {
+				if (lastPage < 6) {
+					for (var i = 1, items = []; i <= lastPage; i += 1) {
+						items.push(i);
+					}
+					html += _private.buildButtons(items);
+				} else if (currentPage + 1 >= lastPage) {
+					html += _private.buildButtons([1, "...", lastPage - 2, lastPage - 1, lastPage]);
+				} else if (currentPage <= 2) {
+					html += _private.buildButtons([1, 2, 3, "...", lastPage]);
+				} else {
+					html += _private.buildButtons([1, "...", currentPage - 1, currentPage, currentPage + 1, "...", lastPage]);
+				}
+			} else {
+				// hide nav if empty results
+				return _options.renderEmptyNavigation();
+			}
 
-                  if(lastPage < 6){
-                      for(var i = 1, items = []; i <= lastPage; i+=1){items.push(i);}
-                      html += _private.buildButtons(items);
-                  }else if((currentPage + 1) >= lastPage){
-                      html += _private.buildButtons([1, "...", lastPage-2, lastPage-1, lastPage]);
-                  }else if(currentPage <= 2){
-                      html += _private.buildButtons([1,2,3,"...", lastPage]);
-                  }else{
-                      html += _private.buildButtons([1,"...", currentPage-1, currentPage, currentPage+1, "...", lastPage]);
-                  }
+			html += "<a href=''class='btn " + _options.btnDefault + "' id='paginator-next-page'>&#62;</a>";
+			html += "</div>";
+			html += "</div></div>";
+			return html;
+		};
+		_private.buildButtons = function(items) {
+			var html = "";
+			for (var i = 0; i < items.length; i += 1) {
+				if (items[i] === "...") {
+					html += "<div class='btn " + _options.btnDefault + "'>...</div>";
+					continue;
+				}
+				html += "<a href='' class='btn " + _options.btnDefault + " paginator-set-page' page='" + items[i] + "'>" + items[i] + "</a>";
+			}
+			return html;
+		};
+		_private.changeActive = function() {
+			$(".paginator-pagination-container ." + _options.btnDefault + "[page='" + _private.page + "']")
+				.removeClass(_options.btnDefault)
+				.addClass(_options.btnActive);
+		};
 
-              }else{
-                  // hide nav if empty results
-                   return _options.renderEmptyNavigation();
-              }
+		_private.run = function() {
+			_private.fetch(function() {
+				_private.buildPage();
+			});
+		};
 
-              html += "<a href=''class='btn " + _options.btnDefault + "' id='paginator-next-page'>&#62;</a>";
-              html+= "</div>";
-              html += "</div></div>";
-              return html;
-        }
-        _private.buildButtons = function(items){
-            var html = "";
-            for(var i = 0; i < items.length; i+=1){
-                if(items[i] === "..."){
-                    html += "<div class='btn " + _options.btnDefault + "'>...</div>";
-                    continue;
-                }
-                html += "<a href='' class='btn " + _options.btnDefault + " paginator-set-page' page='" + items[i] + "'>" + items[i] + "</a>";
-            }
-            return html;
-        }
-        _private.changeActive = function(){
-            $(".paginator-pagination-container ." + _options.btnDefault + "[page='" + _private.page + "']")
-                .removeClass(_options.btnDefault)
-                .addClass(_options.btnActive);
-        }
+		_private.fetch = function(callBack) {
+			_private.setOffset();
 
+			if (_options.data) {
+				_private.parseClient();
+				return callBack();
+			}
+			$.get(_options.url, _private.parseFetchParams())
+				.done(function(results) {
+					var d = _options.parse(results);
+					if (_options.pagination === "client") {
+						_options.data = d;
+						_private.parseClient();
+					} else {
+						_private.currentData = d;
+					}
+					return callBack();
+				})
+				.fail(function(x, h, r) {
+					console.log(r);
+					$.error("fetch : failed to get data from " + _options.url);
+				});
+		};
+		_private.parseClient = function() {
+			var end = _private.offset + _options.pageSize,
+				end = _options.data.length < end ? _options.data.length : end;
+			_private.currentData = {
+				total: _options.data.length,
+				rows: _options.data.slice(_private.offset - 1, end - 1)
+			};
+		};
+		_private.parseFetchParams = function() {
+			if (_options.pagination === "client") return _options.params;
+			return $.extend({}, _options.params, {
+				limit: _options.pageSize,
+				offset: _private.offset
+			});
+		};
 
-        _private.run = function(){
-            _private.fetch(function(){
-                _private.buildPage();
-            });
-        }
+		_private.setLastPage = function() {
+			_private.lastPage = Math.ceil(_private.currentData.total / _options.pageSize);
+		};
+		_private.setOffset = function() {
+			_private.offset = (_private.page - 1) * _options.pageSize + 1;
+		};
 
-        _private.fetch = function(callBack){
-            _private.setOffset();
-
-            if(_options.data){
-                _private.parseClient();
-                return callBack();
-            }
-            $.get(_options.url, _private.parseFetchParams())
-              .done(function(results){
-                  var d = _options.parse(results);
-                  if(_options.pagination === "client"){
-                      _options.data = d;
-                      _private.parseClient();
-                  }else{
-                      _private.currentData = d;
-                  }
-                  return callBack();
-              })
-              .fail(function(x, h, r){
-                  console.log(r);
-                  $.error("fetch : failed to get data from " + _options.url);
-              });
-        }
-        _private.parseClient = function(){
-            var end = _private.offset + _options.pageSize,
-                end = (_options.data.length < end) ? _options.data.length : end;
-            _private.currentData = {
-                total: _options.data.length,
-                rows: _options.data.slice(_private.offset - 1, end - 1)
-            }
-        }
-        _private.parseFetchParams = function(){
-            if(_options.pagination === "client") return _options.params;
-            return $.extend({}, _options.params, {
-                limit: _options.pageSize,
-                offset: _private.offset
-            });
-        }
-
-        _private.setLastPage = function(){
-            _private.lastPage = Math.ceil(_private.currentData.total / _options.pageSize);
-        }
-        _private.setOffset = function(){
-            _private.offset = ((_private.page-1) * _options.pageSize) + 1;
-        }
-
-
-        /**
+		/**
         *   Events
         */
-        $("body").on("click", "#paginator-next-page", function(e){
-            e.preventDefault();
-            _private.$el.paginator("nextPage");
-        });
-        $("body").on("click", "#paginator-prev-page", function(e){
-            e.preventDefault();
-            _private.$el.paginator("prevPage");
-        });
-        $("body").on("click", ".paginator-set-page", function(e){
-            e.preventDefault();
-            _private.$el.paginator("setPage", $(e.currentTarget).attr("page"));
-        });
+		$("body").on("click", "#paginator-next-page", function(e) {
+			e.preventDefault();
+			_private.$el.paginator("nextPage", e);
+		});
+		$("body").on("click", "#paginator-prev-page", function(e) {
+			e.preventDefault();
+			_private.$el.paginator("prevPage", e);
+		});
+		$("body").on("click", ".paginator-set-page", function(e) {
+			e.preventDefault();
+			_private.$el.paginator("setPage", $(e.currentTarget).attr("page"));
+		});
 
-        /**
+		/**
         *   Public functions available through the jquery object
         */
-        return {
-            /**
+		return {
+			/**
             *   Main initialization
             */
-            init: function(options){
-                console.log("init");
+			init: function(options) {
+				console.log("init");
 
-                _private.reset();
-                _private.$el = $(this);
+				_private.reset();
+				_private.$el = $(this);
 
-                //extend defaults with options
-                _options = {};
-                $.extend(_options, _defaults, options);
+				//extend defaults with options
+				_options = {};
+				$.extend(_options, _defaults, options);
 
-                _private.$el.html(_options.renderLoading());
+				_private.$el.html(_options.renderLoading());
 
-                //Validation checking
-                if(!_options.data && !_options.url){
-                    return $.error("init : data/url : [data] or [url] is required");
-                }
+				//Validation checking
+				if (!_options.data && !_options.url) {
+					return $.error("init : data/url : [data] or [url] is required");
+				}
 
-                //preping variables
-                _options.pagination = (_options.pagination !== "server") ? "client" : _options.pagination;
+				//preping variables
+				_options.pagination = _options.pagination !== "server" ? "client" : _options.pagination;
 
-                _private.run();
-            },
+				_private.run();
+			},
 
-            setPage: function(page){
-                console.log("setPage");
-                page = parseInt(page);
-                if(page < 1 || page > _private.lastPage) return $.error("setPage : invalid page number");
-                _private.page = page;
-                _private.run();
-            },
+			setPage: function(page) {
+				console.log("setPage");
+				page = parseInt(page);
+				if (page < 1 || page > _private.lastPage) return $.error("setPage : invalid page number");
+				_private.page = page;
+				_options.onPageChange(e, _private.page);
+				_private.run();
+			},
 
-            nextPage: function(){
-                console.log("nextPage");
-                if(_private.page >= _private.lastPage) return $.error("nextPage : there is no next page");
-                _private.page += 1
-                _private.run();
-            },
+			nextPage: function() {
+				console.log("nextPage");
+				if (_private.page >= _private.lastPage) return $.error("nextPage : there is no next page");
+				_private.page += 1;
+				_options.onPageChange(e, _private.page);
+				_private.run();
+			},
 
-            prevPage: function(){
-                console.log("prevPage");
-                if(_private.page <= 1) return $.error("prevPage : there is no previous page");
-                _private.page -= 1;
-                _private.run();
-            }
-        };
+			prevPage: function() {
+				console.log("prevPage");
+				if (_private.page <= 1) return $.error("prevPage : there is no previous page");
+				_private.page -= 1;
+				_options.onPageChange(e, _private.page);
+				_private.run();
+			}
+		};
+	})();
 
-    }());
-
-
-    /**
+	/**
     *   registration on jquery object
     *       - only routes options to closure methods above
     */
-    $.fn.paginator = function (methodOrOptions) {
-       if (_public[methodOrOptions]) {
-           return _public[methodOrOptions].apply(this, Array.prototype.slice.call(arguments, 1));
-       } else if (typeof methodOrOptions === 'object' || !methodOrOptions) {
-           // Default to "init"
-           return _public.init.apply(this, arguments);
-       } else {
-           $.error('Method ' + methodOrOptions + ' does not exist on jQuery.paginator');
-       }
-    };
-
+	$.fn.paginator = function(methodOrOptions) {
+		if (_public[methodOrOptions]) {
+			return _public[methodOrOptions].apply(this, Array.prototype.slice.call(arguments, 1));
+		} else if (typeof methodOrOptions === "object" || !methodOrOptions) {
+			// Default to "init"
+			return _public.init.apply(this, arguments);
+		} else {
+			$.error("Method " + methodOrOptions + " does not exist on jQuery.paginator");
+		}
+	};
 })(jQuery);
